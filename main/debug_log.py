cf63a89ea80b17822aced6f8f7e8e4bf473fe97c
Artur-Ws <wysywysy97@gmail.com>
2023-04-05 12:05:48 +0200

    Update

---
+++ main/debug_log.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+font = pygame.font.Font(None, 25)
+
+
+def debug(info, x=10, y=10):
+    surface = pygame.display.get_surface()
+    debug_surf = font.render(str(info), True, (255, 255, 255))
+    debug_rect = debug_surf.get_rect(topleft=(x, y))
+    background = pygame.Surface(debug_rect.size)
+    background.set_alpha(80)
+    background.fill((0, 0, 0))
+    surface.blit(background, debug_rect)
+    surface.blit(debug_surf, debug_rect)
