4e25ee87e4a7ec712a4d2678f3ad8cd355f2ff3a
Artur-Ws <wysywysy97@gmail.com>
2023-04-06 16:09:44 +0200

    Add docstring to debug function

--- main/debug_log.py
+++ main/debug_log.py
@@ -4,7 +4,18 @@ pygame.init()
 font = pygame.font.Font(None, 25)
 
 
-def debug(info, y=10, x=10):
+def debug(info, y: int = 10, x: int = 10):
+    """
+    Displays information on screen for debug purposes. To use for dynamic events (i.e. Show actual mouse position),
+    place it in main loop to be updated each frame. For static use (one-time event i.e. Display text after button
+    was pressed), set variable with empty string and put this function in main loop with that variable as info argument,
+    then change this variable after one-time action was performed (i.e.button has been pressed)
+
+    :param info: Information to be displayed on screen.
+    :param y: Y position of displayed text.
+    :param x: X position of displayed text.
+    :return: None
+    """
     surface = pygame.display.get_surface()
     debug_surf = font.render(str(info), True, (255, 255, 255))
     debug_rect = debug_surf.get_rect(topleft=(x, y))
