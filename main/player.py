c504ccf95181c191c9f4c491a3dbf0540cd369f9
Patrycja Wysocka <patrycjawysocka@Patrycjas-MacBook-Air.local>
2023-04-22 21:39:33 +0200

    extended a bit with new methods

--- main/player.py
+++ main/player.py
@@ -1,6 +1,7 @@
 import pygame
 from character import Character
 
+
 class Player(Character):
     def __init__(self, x, y, name, strength, defense, health_points):
         super().__init__(x, y, name, strength, defense, health_points)
@@ -8,19 +9,15 @@ class Player(Character):
         self.backpack_size = 4
         self.level = 1
         self.experience = 0
-        self.experience_needed
-        self.attribiute_points
+        self.experience_needed = 100
+        self.attribiute_points = 10
         self.money = 0
-
-
-    def consume
 
     def add_to_backpack(self, item):
         if len(self.backpack) < self.backpack_size:
             self.backpack.append(item)
         else:
             print("Backpack is full")
-
 
     def remove_from_backpack(self, item):
         is_item = False
@@ -32,9 +29,16 @@ class Player(Character):
         if not is_item:
             print(f"{item} not found in backpack")
 
-
     def expand_backpack(self):
         self.backpack_size += 1
+        print(f"Backpack size has increased to {self.backpack_size}")
 
-    def gain_epxerience
+    def gain_experience(self, gained_experience):
+        self.experience += gained_experience
+        if self.experience >= self.experience_needed:
+            self.level_up()
 
+    def level_up(self):
+        self.level += 1
+        self.experience_needed = int(self.experience_needed * 1.2)
+        print(f"Your actual level: {self.level}")
