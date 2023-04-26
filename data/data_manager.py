238924f646293f2b63869abd1e64dd0718d17f66
Artur-Ws <wysywysy97@gmail.com>
2023-04-26 17:49:56 +0200

    Add environment-enemy association table

--- data/data_manager.py
+++ data/data_manager.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, ForeignKey, Column, String, Integer
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, relationships
 
 Base = declarative_base()
 
@@ -9,18 +9,18 @@ class EnvironmentEnemy(Base):
     env_id = Column("Environment_id", Integer, ForeignKey('Environments.id'))
     enemy_id = Column("Enemy_id", Integer, ForeignKey('Enemies.id'))
 
-    def __init__(self, name, level, category, armor, damage, health, mana, value):
-        self.name = name
-        self.level = level
-        self.category = category
-        self.armor = armor
-        self.damage = damage
-        self.health = health
-        self.mana = mana
-        self.value = value
-
-    def __repr__(self):
-        return f"{self.id}. Item: {self.name}, category: {self.category}, value: {self.value}"
+    # def __init__(self, name, level, category, armor, damage, health, mana, value):
+    #     self.name = name
+    #     self.level = level
+    #     self.category = category
+    #     self.armor = armor
+    #     self.damage = damage
+    #     self.health = health
+    #     self.mana = mana
+    #     self.value = value
+    #
+    # def __repr__(self):
+    #     return f"{self.id}. Item: {self.name}, category: {self.category}, value: {self.value}"
 
     def add_entry(self):
         engine = create_engine("sqlite:///databases/data.db", echo=True)
