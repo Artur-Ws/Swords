084e801fad2fc338a67416609c78122ce2709253
MT <marcin88o@gmail.com>
2023-04-27 19:00:21 +0200

    added requested changes

--- gui/bar.py
+++ gui/bar.py
@@ -1,15 +1,15 @@
 import pygame
 
 
-class Bar():
-    def __init__(self, x, y, points, max_points):
+class Bar:
+    def __init__(self, x, y, max_points, colour_of_background_bar, colour_of_front_bar):
         self.x = x
         self.y = y
-        self.points = points
         self.max_points = max_points
-
-    def draw(self, points, screen, colour_of_background_bar, colour_of_front_bar):
-        self.points = points
-        ratio = self.points / self.max_points
-        pygame.draw.rect(screen, colour_of_background_bar, (self.x, self.y, 150, 20))
-        pygame.draw.rect(screen, colour_of_front_bar, (self.x, self.y, 150 * ratio, 20))
+        self.colour_of_background_bar = colour_of_background_bar
+        self.colour_of_front_bar = colour_of_front_bar
+        self.screen = pygame.display.get_surface()
+    def draw(self, points):
+        ratio = points / self.max_points
+        pygame.draw.rect(self.screen, colour_of_background_bar, (self.x, self.y, 150, 20))
+        pygame.draw.rect(self.screen, colour_of_front_bar, (self.x, self.y, 150 * ratio, 20))
