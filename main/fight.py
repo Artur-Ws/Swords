b48c1e15181bce015a0f4376b078190413b787d2
Pawel Kurowski <mietuski@gmail.com>
2023-04-27 23:23:23 +0200

    Cleaning fight file

--- main/fight.py
+++ main/fight.py
@@ -1,5 +1,4 @@
 from character import Character
-from random import randint
 
 
 class Fight:
@@ -13,21 +12,3 @@ class Fight:
             player.attack(target)
             if target.alive == True:
                 target.attack(player)
-
-    # def choose_environment(self):
-    #     for self.place in self.enemies_from_environemt:
-    #         self.environment_list.append(self.place)
-    #     print(f"Select place where are you want to go {self.environment_list}")
-    #     self.environment_choice = input()
-    #     return self.environment_choice
-    #
-    #
-    # def action(self):
-    #     while True:
-    #         action = input("Select action: Idle - 1 or Attack - 2: ")
-    #         if action == "1" or action == "2":
-    #             return action
-    #         else:
-    #             print("Wrong action, try again")
-
-
