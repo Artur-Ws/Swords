b27e3a73b84183e2bf3b999ddb62f20eddb871fb
cichyc123 <124814739+cichyc123@users.noreply.github.com>
2023-04-27 20:18:55 +0200

    Main menu class refractor and game state/main cleaning (#25)
    * Changing menu_gui to class
    
    * Resolving conflict from game_state
    
    * Cleaning main.py file, removing unnecesary code, because main will inherit more from game state now
    
    * Removing unnecesary code

--- main/main.py
+++ main/main.py
@@ -1,28 +1,15 @@
 import pygame
-import os
 import configparser
-from debug_log import Debug
-from tools import debug_log
 from Game_State import GameState
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-screen_width = config.getint("General", "screen_width")
-screen_height = config.getint("General", "screen_height")
 FPS = config.getint("General", "FPS")
 title = config.get("General", "title")
-
-SCREEN_SIZE = (screen_width, screen_height)
-WIN = pygame.display.set_mode(SCREEN_SIZE)
-BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('..', 'assets', 'background.jpg')), SCREEN_SIZE)
 pygame.display.set_caption(title)
 
 starting_screen = GameState()
-
-
-def draw_window():
-    WIN.blit(BACKGROUND, (0, 0))
 
 
 def main():
@@ -31,13 +18,6 @@ def main():
     while run:
         clock.tick(FPS)
         starting_screen.state_manager()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                run = False
-
-        debug_log()
-        pygame.display.update()
 
     pygame.quit()
 
