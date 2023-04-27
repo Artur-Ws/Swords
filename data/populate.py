da4d195ce0c85e883ae23a8a9d45311c754297c7
Artur-Ws <wysywysy97@gmail.com>
2023-04-27 14:51:53 +0200

    Add instruction of use for populate.py

--- data/populate.py
+++ data/populate.py
@@ -1,32 +1,24 @@
-from enemy import Enemy
-from environments import Environment
-from data_manager import EnvironmentEnemy
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base, relationship
-
-Base = declarative_base()
-engine = create_engine("sqlite:///databases/data.db", echo=True)
-Base.metadata.create_all(bind=engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-
-def add_objects(*args):
-    session.add_all(args)
-    session.commit()
-
-def link_objects(to_be_linked, target_category):
-    target_category.append(to_be_linked)
-    session.commit()
-
-
+from data_manager import add_objects, link_item_enemy, link_environment_enemy, ItemDB, EnemyDB, EnvironmentDB
 '''
-############################################## Don't touch anything above ##############################################
+########################################## ADDING  NEW  OBJECTS  TO  DATABASE ##########################################
 
+To add new objects to database, simply create this objects from DB-version of their classes (imported above).
+Next pass those objects to <add_objects> function. Example below:
 
+enemy1 = EnemyDB("Wolf", 100, 100, 10, 2, 30)
+env1 = EnvironmentDB("Forrest")
+item1 = ItemDB("Wolf Fur", 0, "Neutral", 0, 0, 0, 0, 15)
+item2 = ItemDB("Wolf Fang", 0, "Neutral", 0, 0, 0, 0, 5)
+
+add_objects(enemy1, env1, item1, item2)
+
+################################################### LINKING  OBJECTS ###################################################
+
+To link objects together, those object have to be added to database already. Then just pass names of those objects 
+as an argument to one of specific methods: <link_item_enemy> for connecting item and enemy or <link_environment_enemy>
+for environment and item connection. Example below:
+
+link_environment_enemy("Forrest", "Wolf")
+link_item_enemy("Wolf Fur", "Wolf")
+link_item_enemy("Wolf Fang", "Wolf")
 '''
-
-enemy1 = Enemy("Wolf", 100, 100, 10, 2, 30)
-env1 = Environment("Forrest")
-
-add_objects(enemy1, env1)
