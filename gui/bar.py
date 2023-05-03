21ae977c7517cb2bd979f5e76177f57983565eb8
MT <marcin88o@gmail.com>
2023-05-03 13:47:11 +0200

    updated healthbar

--- gui/bar.py
+++ gui/bar.py
@@ -2,14 +2,13 @@ import pygame
 
 
 class Bar:
-    def __init__(self, x, y, max_points, colour_of_background_bar, colour_of_front_bar):
+    def __init__(self, x, y, colour_of_background_bar, colour_of_front_bar):
         self.x = x
         self.y = y
-        self.max_points = max_points
         self.colour_of_background_bar = colour_of_background_bar
         self.colour_of_front_bar = colour_of_front_bar
         self.screen = pygame.display.get_surface()
-    def draw(self, points):
-        ratio = points / self.max_points
-        pygame.draw.rect(self.screen, colour_of_background_bar, (self.x, self.y, 150, 20))
-        pygame.draw.rect(self.screen, colour_of_front_bar, (self.x, self.y, 150 * ratio, 20))
+    def draw(self, points, max_points):
+        ratio = points / max_points
+        pygame.draw.rect(self.screen, self.colour_of_background_bar, (self.x, self.y, 150, 20))
+        pygame.draw.rect(self.screen, self.colour_of_front_bar, (self.x, self.y, 150 * ratio, 20))
