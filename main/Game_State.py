fc97bddbf18441de3cd26589be66b86c67f9ec29
Patrycja Wysocka <patrycjawysocka@Patrycjas-MacBook-Air.local>
2023-05-04 00:07:23 +0200

    adventure and activity in game_state

--- main/Game_State.py
+++ main/Game_State.py
@@ -5,6 +5,8 @@ from character import Character
 from gui.main_menu_gui import MainMenuGui
 from gui.fight_module_gui import FightModuleGui
 from fight import Fight
+from gui.activity_module import ActivityModuleGui
+from gui.adventure_module import AdventureModuleGui
 
 
 class GameState:
@@ -14,6 +16,10 @@ class GameState:
     def state_manager(self):
         if self.state == 'main_menu':
             self.main_menu()
+        if self.state == 'activity_module':
+            self.activity_module()
+        if self.state == 'adventure_module':
+            self.adventure_module()
         if self.state == 'fight_module':
             self.fight_module()
 
@@ -27,7 +33,7 @@ class GameState:
                     run = False
 
                 if menu.button_play.check_for_input(pygame.mouse.get_pos()):
-                    self.state = 'fight_module'
+                    self.state = 'activity_module'
                     self.state_manager()
 
                 if menu.button_options.check_for_input(pygame.mouse.get_pos()):
@@ -45,7 +51,85 @@ class GameState:
             pygame.display.update()
 
         pygame.quit()
+    def activity_module(self):
+        activity_panel = ActivityModuleGui()
+        run = True
+        while run:
 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    run = False
+
+                if activity_panel.button_adventure.check_for_input(pygame.mouse.get_pos()):
+                    pass
+                    self.state = 'adventure_module'
+                    self.state_manager()
+
+                if activity_panel.button_menu.check_for_input(pygame.mouse.get_pos()):
+                    self.state = 'main_menu'
+                    self.state_manager()
+
+                if activity_panel.button_smithy.check_for_input(pygame.mouse.get_pos()):
+                    pass
+
+                if activity_panel.button_temple.check_for_input(pygame.mouse.get_pos()):
+                    pass
+
+                if activity_panel.button_inn.check_for_input(pygame.mouse.get_pos()):
+                    pass
+
+            activity_panel.draw_fight_module_background()
+            activity_panel.update()
+            activity_panel.check_for_input()
+            activity_panel.change_color()
+
+            debug_log()
+            pygame.display.update()
+
+        pygame.quit()
+
+    def adventure_module(self):
+        adventure_panel = AdventureModuleGui()
+        run = True
+        while run:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    run = False
+
+                if adventure_panel.button_menu.check_for_input(pygame.mouse.get_pos()):
+                    self.state = 'main_menu'
+                    self.state_manager()
+
+                if adventure_panel.button_beach.check_for_input(pygame.mouse.get_pos()):
+                    self.state = 'fight_module'
+                    self.state_manager()
+
+                if adventure_panel.button_ruins.check_for_input(pygame.mouse.get_pos()):
+                    self.state = 'fight_module'
+                    self.state_manager()
+
+                if adventure_panel.button_desert.check_for_input(pygame.mouse.get_pos()):
+                    self.state = 'fight_module'
+                    self.state_manager()
+
+                if adventure_panel.button_cave.check_for_input(pygame.mouse.get_pos()):
+                    self.state = 'fight_module'
+                    self.state_manager()
+
+                if adventure_panel.button_forest.check_for_input(pygame.mouse.get_pos()):
+                    self.state = 'fight_module'
+                    self.state_manager()
+
+            adventure_panel.draw_fight_module_background()
+            adventure_panel.update()
+            adventure_panel.check_for_input()
+            adventure_panel.change_color()
+
+            debug_log()
+            pygame.display.update()
+
+        pygame.quit()
     def fight_module(self):
         fight_panel = FightModuleGui()
         fight = Fight()
