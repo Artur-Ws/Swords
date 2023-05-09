6e249d34ea0d004a5f4a5a1dcbfbc83e581251a5
Patrycja Wysocka <patrycjawysocka@Patrycjas-MacBook-Air.local>
2023-05-09 21:44:06 +0200

    label method

--- gui/label.py
+++ gui/label.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Label():
     def __init__(self, x_pos, y_pos, text_input, font="comicsans", font_size=50):
 
@@ -12,3 +13,6 @@ class Label():
         self.text = self.__button_font.render(self.text_input, True, "yellow", None)
         self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
+    def draw_label(self):
+        surface = pygame.display.get_surface()
+        surface.blit(self.text, self.text_rect)
