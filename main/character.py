e72e7d8a9253bae61c6a63b6afab39d7ec34ad43
MT <marcin88o@gmail.com>
2023-05-11 21:08:47 +0200

    add three attack methods

--- main/character.py
+++ main/character.py
@@ -1,5 +1,6 @@
 import pygame
 from debug_log import Debug
+from random import randint
 
 
 class Character:
@@ -18,12 +19,34 @@ class Character:
         # self.rect = self.image.get_rect()
         # self.rect.center = (x, y)
 
-    def attack(self, target: 'Character'):
+    def light_attack(self, target: 'Character'):
         if self.stamina_check():
-            damage = max(0, self.strength - target.defense)
+            damage = max(0, self.base_damage()*0,5)
             target.get_damage(damage)
             #think about reference of stamina use value depents on type of action, right now it's hardcoded.
+            self.stamina_use(5)
+            print(f"{self.name} attacks {target.name} for {damage} damage! Your stamina lvl: {self.stamina}")
+        else:
+            self.rest()
+            Debug(f"You're exhausted. Get some sleep. Current stamina lvl: {self.stamina}", 2)
+
+    def medium_attack(self, target: 'Character'):
+        if self.stamina_check():
+            damage = max(0, self.base_damage())
+            target.get_damage(damage)
+            # think about reference of stamina use value depents on type of action, right now it's hardcoded.
             self.stamina_use(10)
+            print(f"{self.name} attacks {target.name} for {damage} damage! Your stamina lvl: {self.stamina}")
+        else:
+            self.rest()
+            Debug(f"You're exhausted. Get some sleep. Current stamina lvl: {self.stamina}", 2)
+
+    def heavy_attack(self, target: 'Character'):
+        if self.stamina_check():
+            damage = max(0, self.base_damage()*1,5)
+            target.get_damage(damage)
+            # think about reference of stamina use value depents on type of action, right now it's hardcoded.
+            self.stamina_use(15)
             print(f"{self.name} attacks {target.name} for {damage} damage! Your stamina lvl: {self.stamina}")
         else:
             self.rest()
@@ -62,3 +85,11 @@ class Character:
             self.stamina += self.regen_stamina_value
         else:
             self.stamina = self.stamina_max
+
+    def base_damage(self):
+        base_damage = self.strength * (1 + self.select_chance_draw(-10, 10)/100)
+        return base_damage
+
+    def select_chance_draw(self, first_number_of_draw=1, last_number_of_draw=1000):
+        chance_draw = randint(first_number_of_draw, last_number_of_draw)
+        return chance_draw
