fd3ad88aa708053f649854c82eaf689fce1e73b2
Pawel Kurowski <mietuski@gmail.com>
2023-05-13 07:24:47 +0200

    Make method function_of_block_chance shorter

--- main/character.py
+++ main/character.py
@@ -23,17 +23,23 @@ class Character:
         self.light_attack_multiplier = config.getfloat("FightSettings", "light_attack_multiplier")
         self.medium_attack_multiplier = config.getfloat("FightSettings", "medium_attack_multiplier")
         self.heavy_attack_multiplier = config.getfloat("FightSettings", "heavy_attack_multiplier")
+        self.Attack_Type = Enum("Attack_Type", "strong_attack medium_attack light_attack")
+
         # self.image = pygame.image.load()
         # self.rect = self.image.get_rect()
         # self.rect.center = (x, y)
 
     def attack(self, target: 'Character', type_attack_multiplier=1):
         if self.stamina_check():
-            damage = max(0, self.base_damage() * type_attack_multiplier)
-            target.get_damage(damage)
-            #think about reference of stamina use value depents on type of action, right now it's hardcoded.
-            self.stamina_use(10)
-            print(f"{self.name} attacks {target.name} for {damage} damage! Your stamina lvl: {self.stamina}")
+            if self.is_blocked(target, "medium"):
+                print(f"{target.name} blocked attack ")
+                pass
+            else:
+                damage = max(0, self.base_damage() * type_attack_multiplier)
+                target.get_damage(damage)
+                #think about reference of stamina use value depents on type of action, right now it's hardcoded.
+                self.stamina_use(10)
+                print(f"{self.name} attacks {target.name} for {damage} damage! Your stamina lvl: {self.stamina}")
         else:
             self.rest()
             Debug(f"You're exhausted. Get some sleep. Current stamina lvl: {self.stamina}", 2)
@@ -93,21 +99,18 @@ class Character:
         difference_of_defense_attack = self.defense - opponent.attack
         return difference_of_defense_attack
 
-    def function_of_block_chance(self, opponent: "Character", attack_type: str):
-        if attack_type == "strong":
-            block_chance = config.getint("FightSettings", "equal_block_attack_attack_chance") + \
-                           self.defense_attack_difference(opponent) * \
-                           config.getint("FightSettings","one_point_difference_of_block_attack") + \
-                           config.getint("FightSettings","difference_between_attack_strength")
-        if attack_type == "medium":
-            block_chance = config.getint("FightSettings", "equal_block_attack_attack_chance") + \
+    def function_of_block_chance(self, opponent: "Character", attack_type):
+        medium_attack_block_chance = config.getint("FightSettings", "equal_block_attack_attack_chance") + \
                            self.defense_attack_difference(opponent) * \
                            config.getint("FightSettings","one_point_difference_of_block_attack")
+        if attack_type == self.Attack_Type.strong_attack:
+            block_chance = medium_attack_block_chance + config.getint("FightSettings",
+                                                                      "difference_chance_between_attack_type")
+        if attack_type == "medium":
+            block_chance = medium_attack_block_chance
         if attack_type == "light":
-            block_chance = config.getint("FightSettings", "equal_block_attack_attack_chance") + \
-                           self.defense_attack_difference(opponent) * \
-                           config.getint("FightSettings","one_point_difference_of_block_attack") - \
-                           config.getint("FightSettings","difference_between_attack_strength")
+            block_chance = medium_attack_block_chance - config.getint("FightSettings",
+                                                                      "difference_chance_between_attack_type")
         return block_chance
 
     def is_blocked(self, opponent: "Character", attack_type: str):
