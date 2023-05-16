aefaed31545068f8dc0243f0ea0b03328a6474bf
Artur C <arszil12345@gmail.com>
2023-05-16 21:35:24 +0200

    Adding typing to player class

--- main/player.py
+++ main/player.py
@@ -4,15 +4,15 @@ from character import Character
 
 
 class Player(Character):
-    def __init__(self, x, y, name, strength, defense, health_points):
+    def __init__(self, x, y, name: str, strength: int, defense: int, health_points: int):
         super().__init__(x, y, name, strength, defense, health_points)
-        self.backpack = []
-        self.backpack_size = 12
-        self.level = 1
-        self.experience = 0
-        self.experience_needed = 100
-        self.attribiute_points = 10
-        self.money = 0
+        self.backpack: list = []
+        self.backpack_size: int = 12
+        self.level: int = 1
+        self.experience: int = 0
+        self.experience_needed: int = 100
+        self.attribiute_points: int = 10
+        self.money: int = 0
 
     def add_to_backpack(self, item):
         if len(self.backpack) < self.backpack_size:
@@ -24,7 +24,7 @@ class Player(Character):
         if item in self.backpack:
             self.backpack.remove(item)
 
-    def gain_experience(self, sum_of_enemys_stats):
+    def gain_experience(self, sum_of_enemys_stats: int) -> None:
         self.experience += sum_of_enemys_stats
         while self.experience >= self.experience_needed:
             self.experience -= self.experience_needed
@@ -33,7 +33,5 @@ class Player(Character):
     def level_up(self):
         self.level += 1
         self.experience_needed = int(self.experience_needed * 1.2)
-        self.strength += 2
-        self.defense += 2
         self.health_points_max += 10
         self.health_points = self.health_points_max
