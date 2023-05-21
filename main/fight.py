d9b34bbd6d8f78cf2303fde9277ea7aa11ce1997
Pawel Kurowski <mietuski@gmail.com>
2023-05-21 14:45:32 +0200

    Bug fix

--- main/fight.py
+++ main/fight.py
@@ -1,36 +1,18 @@
 from character import Character, AttackTypeNames
 
 
-attack_type = AttackTypeNames
-
-
 class Fight:
     def __init__(self, player: Character, opponent: Character):
         self.player = player
         self.opponent = opponent
 
-    def fight_action(self):
+    def fight_action(self, attack_type: AttackTypeNames) -> None:
         if self.player.alive and self.opponent.alive:
-            self.player.attack_action(self.opponent, attack_type.medium_attack.value)
+            self.player.attack_action(self.opponent, attack_type.value)
             if self.opponent.alive:
-                self.opponent.attack_action(self.player, attack_type.medium_attack.value)
+                self.opponent.attack_action(self.player, attack_type.value)
 
-    def rest_action(self):
+    def rest_action(self, attack_type: AttackTypeNames) -> None:
         if self.player.alive and self.opponent.alive:
             self.player.rest()
-            self.opponent.attack_action(self.player, attack_type.medium_attack.value)
-
-    # def if_all_alive_action(self, type_attack_method) -> None:
-    #     if self.player.alive and self.opponent.alive:
-    #         type_attack_method()
-    #         if self.opponent.alive:
-    #             self.draw_enemy_action(self.player, self.opponent)
-    #
-    # def draw_enemy_action(selfself.) -> None:
-    #     attack_type_number = randint(1, 3)
-    #     if attack_type_number == 1:
-    #         self.opponent.light_attack(self.player)
-    #     elif attack_type_number == 2:
-    #         self.opponent.medium_attack(self.player)
-    #     else:
-    #         self.opponent.heavy_attack(self.player)
+            self.opponent.attack_action(self.player, attack_type.value)
